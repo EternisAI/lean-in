369ab9fabaa98e9234741eddfd87a24fb5d5d04b
Dmitry Vagner <dmitry.vagner@gmail.com>
2024-11-07 20:53:29 -0800

    changes

--- template/validator/forward.py
+++ template/validator/forward.py
@@ -23,6 +23,7 @@ import bittensor as bt
 from template.protocol import Dummy
 from template.validator.reward import get_rewards
 from template.utils.uids import get_random_uids
+from template.validator.lean_net import make_query
 
 
 async def forward(self):
@@ -39,12 +40,14 @@ async def forward(self):
     # get_random_uids is an example method, but you can replace it with your own.
     miner_uids = get_random_uids(self, k=self.config.neuron.sample_size)
 
+    query = make_query(self.step)
+
     # The dendrite client queries the network.
     responses = await self.dendrite(
         # Send the query to selected miner axons in the network.
         axons=[self.metagraph.axons[uid] for uid in miner_uids],
-        # Construct a dummy query. This simply contains a single integer.
-        synapse=Dummy(dummy_input=self.step),
+        # Construct a dummy query. This simply contains a single string.
+        synapse=Dummy(dummy_input=query),
         # All responses have the deserialize function called on them before returning.
         # You are encouraged to define your own deserialization function.
         deserialize=True,
@@ -55,7 +58,7 @@ async def forward(self):
 
     # TODO(developer): Define how the validator scores responses.
     # Adjust the scores based on responses from miners.
-    rewards = get_rewards(self, query=self.step, responses=responses)
+    rewards = get_rewards(self, query=query, responses=responses)
 
     bt.logging.info(f"Scored responses: {rewards}")
     # Update the scores based on the rewards. You may want to define your own update_scores function for custom behavior.
